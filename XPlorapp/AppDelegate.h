5322ce15cfffc4ce059f095d84c88c456edcd70d
Abner <abner.abbey@hotmail.com>
2016-06-20 17:23:43 -0500

    Initial Commit

---
+++ XPlorapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPlorapp
+//
+//  Created by Abner  Iván Castro Aguilar on 20/06/16.
+//  Copyright © 2016 Abner Iván Castro Aguilar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XPlorapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPlorapp
+//
+//  Created by Abner  Iván Castro Aguilar on 20/06/16.
+//  Copyright © 2016 Abner Iván Castro Aguilar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
